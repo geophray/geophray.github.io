07986f9a7bfb39f9548130481b71540c8fba10b2
geophray <mau17012@byui.edu>
2019-09-17 14:41:29 -0600

    Fixed date last updated in footer.

--- js/last-updated.js
+++ js/last-updated.js
@@ -1,4 +1,13 @@
-var updated = document.lastModified;
-var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-
-document.getElementById("dateUpdated").textContent = updated().toLocaleDateString("en-US", options);
+try {
+    let updated = new Date(document.lastModified);
+    let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    let dateUpdated = updated.toLocaleDateString("en-US", options);
+    document.getElementById("lastUpdated").textContent = dateUpdated;
+} catch (e) {
+    alert("Error with code or your browser does not support Locale.")
+}
