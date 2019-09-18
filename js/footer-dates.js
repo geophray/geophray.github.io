7fa29ed20d7f3451e2646345d097823aef11b239
geophray <mau17012@byui.edu>
2019-09-18 12:08:39 -0600

    Revised copyright year dynamic insertion.

--- js/footer-dates.js
+++ js/footer-dates.js
@@ -11,3 +11,14 @@ try {
 } catch (e) {
     alert("Error with code or your browser does not support Locale.")
 }
+
+try {
+    let currentDate = new Date;
+    let options = {
+        year: 'numeric'
+    };
+    let copyrightYear = currentDate.toLocaleDateString("en-US", options);
+    document.getElementById("copyrightYear").textContent = copyrightYear;
+} catch (e) {
+    alert("Error with code or your browser does not support Locale.")
+}
