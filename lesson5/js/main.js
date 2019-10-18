3e17662d1d7aaade56f496eba5b822195e33170a
geophray <mau17012@byui.edu>
2019-10-18 16:08:42 -0600

    Finished lesson 5 assignment.

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -2,16 +2,30 @@ function toggleMenu() {
 	document.getElementsByClassName("primary-nav")[0].classList.toggle("menu-open");
 }
 
+function displayBanner() {
+	document.getElementById("event_notices").style.display = "block";
+}
+
 try {
-    let currentDate = new Date;
-    let options = {
+	let currentDate = new Date;
+	let options = {
 		weekday: 'long',
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric'
 	};
-    let formattedDate = currentDate.toLocaleDateString("en-GB", options);
-    document.getElementById("todaysDate").textContent = formattedDate;
+	let formattedDate = currentDate.toLocaleDateString("en-GB", options);
+	document.getElementById("todaysDate").textContent = formattedDate;
 } catch (e) {
-    alert("Error with code or your browser does not support Locale.")
+	alert("Error with code or your browser does not support Locale.")
+}
+
+try {
+	let currentDate = new Date;
+	let currentDay = currentDate.getDay();
+	if (currentDay === 5) {
+		displayBanner();
+	}
+} catch (e) {
+	alert("Error with code or your browser does not support Locale.")
 }
