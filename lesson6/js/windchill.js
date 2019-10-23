39a5237e4d7b3e0399b1559e920a287f5e84e9bb
geophray <mau17012@byui.edu>
2019-10-23 10:34:57 -0600

    Updated wind chill calculation.

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,7 +3,7 @@ let s = parseFloat(document.getElementById("wind_speed").innerText);
 
 const windChillCalc = (t, s) => {
     let windChill = 'N/A';
-    if (t <= 50 || s > 3.0) {
+    if (t <= 50 && s > 3.0) {
         windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
         return windChill.toFixed(1) + " &deg;F";
     } else {
